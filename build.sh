106d647947b7e9e13c9827a3086535815f1c89f9
xbanki <contact@xbanki.me>
2021-11-07 18:09:26 +0200

    feat: Change build script shell `sh -> bash`

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 if [[ $(git rev-parse --abbrev-ref HEAD) == "main" || $(git log --pretty='format:%Creset%s' --no-merges -1) =~ "(vercel-build)" ]] ;
-    then exit 1; else exit 0;
+    then
+        exit 1;
+    else
+        exit 0;
 fi
