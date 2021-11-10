3534d14992de9b1537dffe2f097ecfa125ab82ba
xbanki <contact@xbanki.me>
2021-11-10 03:55:10 +0200

    feat(vercel-build): Add background image fetch

--- src/vue/background/background.ts
+++ src/vue/background/background.ts
@@ -1,7 +1,7 @@
+import { DateTime }        from 'luxon';
 import { defineComponent } from 'vue';
-import flatted from 'flatted';
 
-import   axios from 'axios';
+import axios from 'axios';
 
 import { GetDailyBackgroundData } from '@/lib/http';
 
@@ -11,7 +11,25 @@ import config from '@/lib/config';
  * Component data internal description interface.
  */
 interface ComponentData {
+
+    /**
+     * Daily image raw data.
+     * @type {GetDailyBackgroundData}
+     * @see {GetDailyBackgroundData}
+     */
     image_data: undefined | GetDailyBackgroundData;
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Is the img element loading.
+     * @type {boolean}
+     */
+    loading: boolean;
 }
 
 export default defineComponent({
@@ -19,6 +37,43 @@ export default defineComponent({
     data() {
         const data: ComponentData = { image_data: undefined };
 
-        return { data };
-    }
+        const state: ComponentState = { loading: true };
+
+        return { state, data };
+    },
+
+    mounted() {
+        this.$nextTick(() => {
+
+            axios.get(`${config.api_url}/get-daily-background`)
+
+                // Parse image data
+                .then(data => data.data as GetDailyBackgroundData)
+                .then(
+                    (data) => {
+                        const expires_on = DateTime.fromISO(data.data.expires_on as unknown as string);
+
+                        return { ...data, data: { ...Object.assign(data.data, { expires_on }) }};
+                    }
+                )
+                .then(
+                    (data) => {
+                        if (!data) return;
+
+                        const image = this.$refs.image as HTMLImageElement;
+
+                        image.crossOrigin = 'Anonymous';
+                        image.src = data.data.url;
+
+                        // Handle loaded event
+                        image.onload = () => {
+                            if (image.complete || image.complete === undefined) {
+                                this.state.loading = false;
+                                return;
+                            }
+                        };
+                    }
+                );
+        });
+    } 
 });
--- src/vue/background/background.vue
+++ src/vue/background/background.vue
@@ -1,7 +1,11 @@
 <template>
 
     <!-- Background element display wrapper //-->
-    <main class="component-background"/>
+    <main class="component-background">
+
+        <!-- Dynamically rendered background image //-->
+        <img ref="image" v-show="!state.loading" alt="Background Image">
+    </main>
 </template> 
 
 <script lang="ts" src="./background.ts"></script>
