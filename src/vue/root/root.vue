aaab92664ed3e01b436f531b0f50e3a4aa259267
xbanki <contact@xbanki.me>
2021-11-11 18:49:56 +0200

    feat(vercel-build): Add debug buttons

--- src/vue/root/root.vue
+++ src/vue/root/root.vue
@@ -2,7 +2,14 @@
     <background-component/>
 
     <!-- Content display master element //-->
-    <main class="component-root"/>
+    <main class="component-root">
+
+        <teleport to="main#go-xbanki-ui">
+            <button v-on:click="debug_stretch"> Stretch </button>
+            <button v-on:click="debug_fit"> Fit </button>
+            <button v-on:click="debug_fill"> Fill </button>
+        </teleport>
+    </main>
 </template>
 
 <script lang="ts" src="./root.ts"></script>
