88b38c3585a9473fbf5f21eb337c1d69d879edba
xbanki <contact@xbanki.me>
2021-11-13 00:53:48 +0200

    feat: Start work on `modal` component

---
+++ src/vue/modal/modal.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <!-- Modal UI layer anchor //-->
+    <teleport to="main#go-xbanki-ui">
+
+        <!-- Modal component positioning wrapper //-->
+        <main class="component-modal"/>
+    </teleport>
+</template>
+
+<script lang="ts" src="./modal.ts"></script>
+<style lang="scss" src="./modal.scss"></style>
