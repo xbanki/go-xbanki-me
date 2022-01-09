73f3d36845e74ec2b0e703beb3937dc6ece0ab49
xbanki <contact@xbanki.me>
2022-01-10 01:43:57 +0200

    feat: Add persistence plugin use

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -4,7 +4,7 @@ import eventBusStore from '@/lib/store_event_bus';
 import settingsStore from '@/lib/store_settings';
 import persistence   from '@/lib/persistence';
 
-const plugins = [ persistence({ application_name: 'go-xbanki-me' }) ];
+const plugins = [ persistence({ application_name: 'go-xbanki-me', namespaces: ['settingsStore'] }) ];
 
 const modules = {
     settingsStore,
