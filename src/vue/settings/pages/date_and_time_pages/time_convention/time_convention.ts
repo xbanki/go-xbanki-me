e84a810564145d2bc7ee7cff2a795421d218d054
xbanki <contact@xbanki.me>
2022-06-09 18:10:16 +0300

    feat: Re-Implement time convention option

--- src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.ts
+++ src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.ts
@@ -1,3 +1,41 @@
+import { Store, mapState } from 'vuex';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import { ClockConvention, ModuleState } from '@/lib/store_settings';
+
+import store from '@/lib/store';
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Active clock display convention.
+     * @type {ClockConvention}
+     */
+    selected_clock_convention: ClockConvention;
+}
+
+export default defineComponent({
+
+    data() {
+        const typed_store = store as Store<{ settingsStore: ModuleState }>;
+
+        const selected_clock_convention = typed_store.state.settingsStore.time_convention;
+
+        const state: ComponentState = { selected_clock_convention };
+
+        return { state };
+    },
+
+    methods: {
+        update() {
+            if (this.settingsStore.time_convention != this.state.selected_clock_convention) {
+                store.commit('settingsStore/SET_CLOCK_CONVENTION', this.state.selected_clock_convention);
+            }
+        }
+    },
+
+    computed: mapState(['settingsStore'])
+});
--- src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.vue
+++ src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.vue
@@ -1,6 +1,35 @@
 <template>
 
     <!-- Time convention category page root element //-->
+    <main class="component-time-convention">
+
+        <!-- Time Convention component title display //-->
+        <span class="time-convention-title"> Time Convention </span>
+
+        <!-- Time Convention selections container //-->
+        <section class="time-convention-selections">
+
+            <!-- Time Convention item container //-->
+            <div class="selections-item">
+
+                <!-- Time Convention selection icon //-->
+                <input type="radio" name="item-european" id="time-convention-european" value="CONVENTION_24H" v-on:change="update" v-model="state.selected_clock_convention">
+
+                <!-- Time Convention name descriptor //-->
+                <label for="item-european"> 24 Hour Clock </label>
+            </div>
+
+            <!-- Time Convention item container //-->
+            <div class="selections-item">
+
+                <!-- Time Convention selection icon //-->
+                <input type="radio" name="item-american" id="time-convention-american" value="CONVENTION_AM_PM" v-on:change="update" v-model="state.selected_clock_convention">
+
+                <!-- Time Convention name descriptor //-->
+                <label for="item-american"> 12 Hour Clock (AM/PM) </label>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script lang="ts" src="./time_convention.ts"></script>
