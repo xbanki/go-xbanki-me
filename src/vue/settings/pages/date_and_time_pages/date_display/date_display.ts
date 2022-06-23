9695ad18ce658f19b16c10a0b41d5b832eaca2f5
xbanki <contact@xbanki.me>
2022-06-23 22:52:53 +0300

    feat: Add date delimiter option & styles

--- src/vue/settings/pages/date_and_time_pages/date_display/date_display.ts
+++ src/vue/settings/pages/date_and_time_pages/date_display/date_display.ts
@@ -1,3 +1,85 @@
+import { Store, mapState } from 'vuex';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import { ModuleState, FormatDelimiter, FormatToken } from '@/lib/store_settings';
+
+import store from '@/lib/store';
+
+/**
+ * Active format state.
+ */
+interface ComponentStateFormat {
+
+    /**
+     * Active time display delimiter.
+     * @enum {FormatDelimiter}
+     */
+    delimiter: FormatDelimiter;
+
+    /**
+     * Currently active format.
+     * @type {Array<FormatToken>}
+     */
+    active: FormatToken[];
+
+    /**
+     * Inactive format tokens.
+     * @type {Array<FormatToken>}
+     */
+    inactive: FormatToken[];
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Active format data.
+     * @type {ComponentStateFormat}
+     */
+    format: ComponentStateFormat;
+}
+
+export default defineComponent({
+
+    data() {
+        const typed_store = store as Store<{ settingsStore: ModuleState }>;
+
+        // Format object state
+
+        const delimiter = typed_store.state.settingsStore.date_delimiter;
+
+        const inactive = typed_store.state.settingsStore.date_format_inactive;
+
+        const active = typed_store.state.settingsStore.date_format_active;
+
+        const format: ComponentStateFormat = {
+            delimiter,
+            inactive,
+            active
+        };
+
+        const state: ComponentState = {
+            format
+        };
+
+        return { state };
+    },
+
+    methods: {
+        update_delimiter() {
+
+            if (this.state.format.delimiter != this.settingsStore.time_delimiter) {
+
+                store.commit('settingsStore/UPDATE_DATE_FORMAT_DELIMITER', this.state.format.delimiter);
+
+                this.update_active_format();
+            }
+        },
+
+        update_active_format() { store.dispatch('settingsStore/SetDateFormat', [this.state.format.active, this.state.format.inactive]); }
+    },
+
+    computed: mapState(['settingsStore'])
+});
--- src/vue/settings/pages/date_and_time_pages/date_display/date_display.vue
+++ src/vue/settings/pages/date_and_time_pages/date_display/date_display.vue
@@ -1,6 +1,75 @@
 <template>
 
     <!-- Date display category page root element //-->
+    <main class="component-date-display">
+
+        <!-- Date Display component title display //-->
+        <span class="date-display-title"> Date Display </span>
+
+        <!-- Delimiter choise section //-->
+        <section class="date-display-delimiter">
+
+            <!-- Space delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-space" id="delimiter-space" value="DELIMITER_SPACE" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-space"> Blank </label>
+            </div>
+
+            <!-- Comma delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-comma" id="delimiter-comma" value="DELIMITER_COMMA" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-comma"> Comma </label>
+            </div>
+
+            <!-- Slash delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-slash" id="delimiter-slash" value="DELIMITER_SLASH" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-slash"> Slash </label>
+            </div>
+
+            <!-- Dash delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-dash" id="delimiter-dash" value="DELIMITER_DASH" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-dash"> Dash </label>
+            </div>
+
+            <!-- Dot delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-dot" id="delimiter-dot" value="DELIMITER_DOT" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-dot"> Period </label>
+            </div>
+
+            <!-- Colon delimiter //-->
+            <div class="selections-item">
+
+                <!-- Radio input element //-->
+                <input type="radio" name="delimiter-colon" id="delimiter-colon" value="DELIMITER_COLON" v-on:change="update_delimiter" v-model="state.format.delimiter">
+
+                <!-- Radio label element //-->
+                <label for="delimiter-colon"> Colon </label>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script lang="ts" src="./date_display.ts"></script>
