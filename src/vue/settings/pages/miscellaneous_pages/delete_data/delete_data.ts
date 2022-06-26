8c7accdd13da3df522312529c89e8e54a44dbf9b
xbanki <contact@xbanki.me>
2022-06-26 15:28:42 +0300

    feat: Begin work on `Delete Data` section

--- src/vue/settings/pages/miscellaneous_pages/delete_data/delete_data.ts
+++ src/vue/settings/pages/miscellaneous_pages/delete_data/delete_data.ts
@@ -1,3 +1,102 @@
+import { mapState }        from 'vuex';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+/**
+ * Delete button labels.
+ */
+enum DeleteButtonLabel {
+    INITIAL = 'Delete Saved Data',
+    CONFIRM = 'Confirm?'
+}
+
+/**
+ * Backup button labels.
+ */
+enum BackupButtonLabel {
+    INITIAL = 'Backup Saved Data',
+    CONFIRM = 'Confirm?'
+}
+
+/**
+ * Display label states.
+ */
+interface ComponentStateDisplay {
+
+    /**
+     * Delete data button label.
+     * @enum {DeleteButtonLabel}
+     */
+    remove: DeleteButtonLabel;
+
+    /**
+     * Backup saved data button label.
+     * @enum {BackupButtonLabel}
+     */
+    backup: BackupButtonLabel;
+
+    /**
+     * Restore backed up data button label.
+     * @type {string}
+     */
+    restore: string;
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Button label state.
+     * @type {ComponentStateDisplay}
+     */
+    display: ComponentStateDisplay;
+}
+
+export default defineComponent({
+
+    data() {
+
+        // Display state defaults
+
+        const remove = DeleteButtonLabel.INITIAL;
+
+        const backup = BackupButtonLabel.INITIAL;
+
+        const restore = 'Restore Saved Data';
+
+        // Final assembled state objects
+
+        const display: ComponentStateDisplay = {
+            restore,
+            remove,
+            backup
+        };
+
+        const state: ComponentState = {
+            display
+        };
+
+        return { state };
+    },
+
+    methods: {
+
+        /**
+         * Handles deletion of all user-saved cookies.
+         */
+        handle_click_delete() { this; },
+
+        /**
+         * Handles downloading all persisted data into a singular JSON file.
+         */
+        handle_click_backup() { this; },
+
+        /**
+         * Handles loading backup JSON file & setting the appropriate data.
+         */
+        handle_click_restore() { this; }
+    },
+
+    computed: mapState(['settingsStore', '__metaData'])
+});
--- src/vue/settings/pages/miscellaneous_pages/delete_data/delete_data.vue
+++ src/vue/settings/pages/miscellaneous_pages/delete_data/delete_data.vue
@@ -1,6 +1,24 @@
 <template>
 
     <!-- Delete data category page root element //-->
+    <main class="component-delete-data">
+        
+        <!-- Delete Data component title display //-->
+        <span class="delete-data-title"> Delete Data </span>
+
+        <!-- Delete data buttons wrapper //-->
+        <section class="delete-data-buttons">
+
+            <!-- Delete data itself //-->
+            <button class="buttons-delete" v-on:click="handle_click_delete"> {{ state.display.remove }} </button>
+
+            <!-- Backup all active data //-->
+            <button class="buttons-backup" v-on:click="handle_click_backup"> {{ state.display.backup }} </button>
+
+            <!-- Restore backup data //-->
+            <button class="buttons-restore" v-on:click="handle_click_restore"> {{ state.display.restore }} </button>
+        </section>
+    </main>
 </template>
 
 <script lang="ts" src="./delete_data.ts"></script>
