06ed9b086b268c0c02be1a224d954bf5f995cb52
xbanki <contact@xbanki.me>
2022-07-02 03:14:34 +0300

    feat: Add base changelog displays

--- src/vue/settings/pages/miscellaneous_pages/changelog/changelog.ts
+++ src/vue/settings/pages/miscellaneous_pages/changelog/changelog.ts
@@ -1,3 +1,73 @@
 import { defineComponent } from 'vue';
+import { DateTime } from 'luxon';
 
-export default defineComponent({});
+import JanTenth from './articles/jan-10_0-2-0.md';
+import JanFifth from './articles/jan-5_0-1-0.md';
+
+/**
+ * Single changelog Markdown article.
+ */
+interface ChangelogArticle {
+
+    /**
+     * Version this article represents.
+     * @type {string}
+     */
+    version: string;
+
+    /**
+     * Date of which this update was released.
+     * @type {string}
+     */
+    date: DateTime;
+
+    /**
+     * Rendered Markdown HTML.
+     * @type {string}
+     */
+    html: string;
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Current update changelog.
+     * @type {ChangelogArticle}
+     */
+    latest: ChangelogArticle;
+
+    /**
+     * History of all older updates.
+     * @type {Array<ChangelogArticle>}
+     */
+    legacy: ChangelogArticle[];
+}
+
+const GIT_COMMIT_FORMAT = 'EEE MMM dd HH:mm:ss yyyy ZZZ';
+
+export default defineComponent({
+    data() {
+
+        const latest: ChangelogArticle = { version: JanTenth.metadata.version, date: DateTime.fromFormat(JanTenth.metadata.date, GIT_COMMIT_FORMAT), html: JanTenth.html };
+
+        const legacy: ChangelogArticle[] = [
+            { version: JanFifth.metadata.version, date: DateTime.fromFormat(JanFifth.metadata.date, GIT_COMMIT_FORMAT), html: JanFifth.html }
+        ];
+
+        // Assembled state component
+
+        const state: ComponentState = {
+            legacy,
+            latest
+        };
+
+        return { state };
+    },
+
+    methods: {
+        get_styled_datetime: (input: DateTime) => `On ${ input.toFormat('f') }`
+    }
+});
--- src/vue/settings/pages/miscellaneous_pages/changelog/changelog.vue
+++ src/vue/settings/pages/miscellaneous_pages/changelog/changelog.vue
@@ -1,6 +1,24 @@
 <template>
 
     <!-- Changelog category page root element //-->
+    <main class="component-changelog">
+
+        <!-- Changelog component title display //-->
+        <span class="changelog-title"> Changelog </span>
+
+        <!-- Latest update changelog item //-->
+        <section class="changelog-latest">
+
+            <!-- Rendered Markdown //-->
+            <div class="latest-markdown" v-html="state.latest.html"/>
+
+            <!-- Release marker //-->
+            <span class="changelog-release"> {{ get_styled_datetime(state.latest.date) }} </span>
+        </section>
+
+        <!-- History of older versions //-->
+        <section class="changelog-history"/>
+    </main>
 </template>
 
 <script lang="ts" src="./changelog.ts"></script>
