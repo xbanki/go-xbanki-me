10384c012538c5de7a1f27f52dbe3501407d5028
xbanki <contact@xbanki.me>
2022-07-03 01:21:30 +0300

    feat: Add skeleton for `Privacy & Safety` component

--- src/vue/settings/pages/miscellaneous_pages/privacy_and_safety/privacy_and_safety.ts
+++ src/vue/settings/pages/miscellaneous_pages/privacy_and_safety/privacy_and_safety.ts
@@ -1,3 +1,17 @@
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+
+    methods: {
+
+        /**
+         * Handles click for navigating to cookie usage page.
+         */
+        navigate_cookies() { this; },
+
+        /**
+         * Handles click navigation for all FOSS licenses list.
+         */
+        navigate_licenses() { this; }
+    }
+});
--- src/vue/settings/pages/miscellaneous_pages/privacy_and_safety/privacy_and_safety.vue
+++ src/vue/settings/pages/miscellaneous_pages/privacy_and_safety/privacy_and_safety.vue
@@ -1,6 +1,21 @@
 <template>
 
     <!-- Privacy & safety category page root element //-->
+    <main class="privacy-and-safety">
+
+        <!-- Privacy & Safety section titlebar //-->
+        <span class="privacy-and-safety-title"> Privacy & Safety </span>
+
+        <!-- Privacy & Safety navigation buttons //-->
+        <section class="privacy-and-safety-buttons">
+
+            <!-- Cookie usage button //-->
+            <button class="buttons-cookies" v-on:click="navigate_cookies"> Cookie Usage </button>
+
+            <!-- FOSS licenses //-->
+            <button class="buttons-licenses" v-on:click="navigate_licenses"> Licenses </button>
+        </section>
+    </main>
 </template>
 
 <script lang="ts" src="./privacy_and_safety.ts"></script>
