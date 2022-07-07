3144b9056fd325efe3b58618fb7680fdea927190
xbanki <contact@xbanki.me>
2022-07-07 03:16:47 +0300

    feat: Add licenses display component

--- src/vue/settings/pages/miscellaneous_pages/licenses/licenses.ts
+++ src/vue/settings/pages/miscellaneous_pages/licenses/licenses.ts
@@ -1,3 +1,116 @@
+import { marked }          from 'marked';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+
+import dependency_licenses from 'virtual:licenses';
+
+/**
+ * License item which we should render in the component.
+ */
+interface ComponentStateLicense {
+
+    /**
+     * Denotes wether the dropdown item is active or not.
+     * @type {boolean}
+     */
+    active: boolean;
+
+    /**
+     * Name of the package this item is for.
+     * @type {string}
+     */
+    name: string;
+
+    /**
+     * Un-rendered license data.
+     * @type {string}
+     */
+    content: string;
+
+    /**
+     * License object's home URL.
+     * @type {string}
+     */
+    home?: string;
+
+    /**
+     * Package author.
+     * @type {string}
+     */
+    author?: string;
+
+    /**
+     * Repository URL for the source code.
+     * @type {string}
+     */
+    repo?: string;
+
+    /**
+     * Author contact email.
+     * @type {string}
+     */
+    email?: string;
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * A list of all licenses that we use.
+     * @type {LicenseItem}
+     */
+    licenses: ComponentStateLicense[];
+}
+
+function prepare_license_data(): ComponentStateLicense[] {
+
+    const result: ComponentStateLicense[] = [];
+
+    for (const target of Object.values(dependency_licenses)) {
+
+        if (!target.text || !target.license) continue;
+
+
+        const content = target.text;
+
+        const author = target.author;
+
+        const email = target.email;
+
+        const repo = target.repository;
+
+        const name = target.name;
+
+        const home = target.url;
+
+        const active = false;
+
+        result.push({ author, active, content, email, home, name, repo });
+    }
+
+    return result;
+}
+
+export default defineComponent({
+
+    data() {
+
+        const licenses: ComponentStateLicense[] = prepare_license_data();
+
+        // Assembled state object
+
+        const state: ComponentState = { licenses };
+
+        return { state };
+    },
+
+    methods: {
+        toggle_dropdown_state: (target: ComponentStateLicense) => target.active ? target.active = false : target.active = true,
+
+        render_content: (content: string) => marked(content),
+
+        get_styled_author: (author: string) => `By: ${author}`
+    }
+});
--- src/vue/settings/pages/miscellaneous_pages/licenses/licenses.vue
+++ src/vue/settings/pages/miscellaneous_pages/licenses/licenses.vue
@@ -1,6 +1,57 @@
 <template>
 
     <!-- FOSS licenses component //-->
+    <main class="component-licenses">
+
+        <!-- Auto-generated license items //-->
+        <section
+        
+            class="licenses-item"
+            
+            v-for="item of state.licenses"
+            
+            v-bind:class="{ active: item.active }"
+
+            v-bind:key="state.licenses.indexOf(item)"
+        >
+            
+            <!-- License item titlebar //-->
+            <div class="item-title" v-on:click="toggle_dropdown_state(item)">
+
+                <!-- State chevron display inactive //-->
+                <span class="title-arrow" v-if="!item.active"> ▼ </span>
+
+                <!-- State chevron display active //-->
+                <span class="title-arrow" v-else> ▲ </span>
+
+                <!-- Package name //-->
+                <span class="title-name"> {{ item.name }} </span>
+
+                <!-- Package home link //-->
+                <a class="title-home" v-if="item.home" v-bind:href="item.home"> Home </a>
+            </div>
+
+            <!-- Rendered license content //-->
+            <div class="item-content" v-if="item.active">
+                
+                <!-- Owner information rail //-->
+                <div class="content-upper" v-if="item.author || item.email || item.repo">
+
+                    <!-- Creator name label //-->
+                    <span class="upper-name" v-if="item.author"> {{ get_styled_author(item.author) }} </span>
+
+                    <!-- Creator contact email //-->
+                    <a class="upper-email" v-if="item.email" v-bind:href="`mailto:${item.email}`"> E-Mail </a>
+
+                    <!-- Code repository link //-->
+                    <a class="upper-repo" v-bind:href="item.repo" v-if="item.repo"> Code </a>
+                </div>
+
+                <!-- Markdown rendered license display //-->
+                <div class="content-display" v-html="render_content(item.content)"/>
+            </div>
+        </section>
+    </main>
 </template>
 
 <script lang="ts" src="./licenses.ts"></script>
