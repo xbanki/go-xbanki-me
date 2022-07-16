72e874af6f62b74986594f9924f7b74a2887e202
xbanki <contact@xbanki.me>
2022-07-17 02:24:34 +0300

    fix: Clock convention update not updating format

--- src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.ts
+++ src/vue/settings/pages/date_and_time_pages/time_convention/time_convention.ts
@@ -1,7 +1,7 @@
 import { Store, mapState } from 'vuex';
 import { defineComponent } from 'vue';
 
-import { ClockConvention, ModuleState } from '@/lib/store_settings';
+import { ClockConvention, ModuleState, FormatToken } from '@/lib/store_settings';
 
 import store from '@/lib/store';
 
@@ -32,6 +32,28 @@ export default defineComponent({
     methods: {
         update() {
             if (this.settingsStore.time_convention != this.state.selected_clock_convention) {
+
+                // Denotes wether or not the meridem token is in active format.
+                let meridem_index: number | undefined;
+
+                for (const target of this.settingsStore.time_format_active as FormatToken[]) if (target?.token == 'a') {
+
+                    meridem_index = this.settingsStore.time_format_active.indexOf(target);
+
+                    break;
+                }
+
+                // Handle format update
+                if (meridem_index != undefined) {
+
+                    const inactive: FormatToken[] = this.settingsStore.time_format_inactive;
+                    const active:   FormatToken[] = this.settingsStore.time_format_active;
+
+                    active[meridem_index] = Object.assign(active[meridem_index], { disabled: this.state.selected_clock_convention == ClockConvention.EUROPEAN ? true : false });
+
+                    store.dispatch('settingsStore/SetTimeFormat', [active, inactive]);
+                }
+
                 store.commit('settingsStore/SET_CLOCK_CONVENTION', this.state.selected_clock_convention);
             }
         }
