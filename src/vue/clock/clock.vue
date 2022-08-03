20d4c3868ff7253038efcde4db90e8ca366f7c47
xbanki <contact@xbanki.me>
2022-08-03 13:55:29 +0300

    chore: Add missing CSS & HTML updates

--- src/vue/clock/clock.vue
+++ src/vue/clock/clock.vue
@@ -3,11 +3,8 @@
     <!-- Clock component wrapper parent //-->
     <main class="component-clock">
 
-        <!-- Clock date display //-->
-        <span class="clock-date"> {{ state.active_time_data }} </span>
-
         <!-- CLock current time display //-->
-        <span class="clock-time"> {{ state.active_date_data }} </span>
+        <span class="clock-time"> {{ state.active_time_data }} </span>
     </main>
 </template>
 
