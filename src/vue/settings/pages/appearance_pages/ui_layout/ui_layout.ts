feaad8bd013a56a170ec5e8d30eadd0f02dafc81
xbanki <contact@xbanki.me>
2022-08-05 20:20:14 +0300

    feat: Add base UI for layout in settings

--- src/vue/settings/pages/appearance_pages/ui_layout/ui_layout.ts
+++ src/vue/settings/pages/appearance_pages/ui_layout/ui_layout.ts
@@ -1,3 +1,77 @@
+import { Store, mapState } from 'vuex';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+import { ModuleState } from '@/lib/store_event_bus';
+
+import store from '@/lib/store';
+
+/**
+ * Disabled state data.
+ */
+interface ComponentStateDisabled {
+
+    /**
+     * Disables the 'Edit UI Layout' button.
+     * @type {boolean}
+     */
+    edit: boolean;
+
+    /**
+     * Disables resetting UI layout.
+     * @type {boolean}
+     */
+    reset: boolean;
+}
+
+/**
+ * Comonent internal state.
+ */
+interface ComponentState {
+
+    /**
+     * Button disable state.
+     * @type {ComponentStateDisabled}
+     */
+    disable: ComponentStateDisabled;
+}
+
+export default defineComponent({
+
+    data() {
+
+        const typed_store = store as Store<{ eventBusStore: ModuleState }>;
+
+        // Disable state constants
+
+        // @TODO(xbanki): We should get this if layout has been marked dirty
+        const reset = true;
+
+        const edit = typed_store.state.eventBusStore.supports_data_persistence;
+
+        // Final state objects
+
+        const disable: ComponentStateDisabled = {
+            reset,
+            edit
+        };
+
+        const state: ComponentState = {
+            disable
+        };
+
+        return { state };
+    },
+
+    methods: {
+
+        handle_click_edit() { this; },
+
+        handle_click_reset() { this; }
+    },
+
+    watch: {
+        'eventBusStore.supports_data_persistence'(state: boolean) { this.state.disable.edit = state; }
+    },
+
+    computed: mapState(['eventBusStore'])
+});
--- src/vue/settings/pages/appearance_pages/ui_layout/ui_layout.vue
+++ src/vue/settings/pages/appearance_pages/ui_layout/ui_layout.vue
@@ -5,6 +5,16 @@
 
         <!-- UI Layout component title display //-->
         <span class="ui-layout-title"> UI Layout </span>
+
+        <!-- UI layout control buttons //-->
+        <section class="ui-layout-buttons">
+            
+            <!-- Edit layout button //-->
+            <button class="buttons-edit" v-on:click="handle_click_edit" v-bind:disabled="!state.disable.edit"> Edit UI Layout </button>
+
+            <!-- Reset layout button //-->
+            <button class="buttons-reset" v-on:click="handle_click_reset" v-bind:disabled="state.disable.reset"> Reset UI Layout </button>
+        </section>
     </main>
 </template>
 
